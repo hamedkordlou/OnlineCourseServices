1fcc70b43dde1c87e993ed04a6fc8ac4fd2a886b
hamed <hamed@DESKTOP-5SUPPEI>
2018-09-16 19:27:54 +0430

    Add project files.

---
+++ OnlineCourseServices/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OnlineCourseServices.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ OnlineCourseServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineCourseServices.WebApiApplication" Language="C#" %>
